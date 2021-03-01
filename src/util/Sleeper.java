55ea0df6e2e4675d288daaf83e478efc1aba6c9f
Jouke Stoel <j.h.stoel@gmail.com>
2021-03-01 16:45:22 +0100

    Removed old banking example

---
+++ src/util/Sleeper.java
@@ -0,0 +1,18 @@
+package util;
+
+import io.usethesource.vallang.IInteger;
+import io.usethesource.vallang.IValueFactory;
+
+public class Sleeper {
+
+	public Sleeper(IValueFactory vf) {}
+	
+	public void sleep(IInteger ms) {
+		try {
+			Thread.sleep(ms.longValue());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}		
+	}
+}
+ 
